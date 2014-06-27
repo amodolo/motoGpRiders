04c715a2854a98e076d4b26c16ff9cd4544f7691
alessandro <alessandro.modolo.1988@gmail.com>
2014-06-27 07:44:07 +0200

    STEP3  - modificato il model aggiungendo la lista di piloti  - utlizzata la direttiva ng-repeat

--- controller.js
+++ controller.js
@@ -1,5 +1,42 @@
-var mainController = function($scope){
-    $scope.rider = {
-        name: "Valentino Rossi"
+var riders = [
+    {
+        name: "Valentino Rossi",
+        number: 46,
+        team: "Movistar Yamaha MotoGP",
+        nation: 'ita',
+        height: 182,
+        weight: 65,
+        city: "Urbino"
+    },
+    {
+        name: "Jorge Lorenzo",
+        number: 99,
+        team: "Movistar Yamaha MotoGP",
+        nation: 'esp',
+        height: 173,
+        weight: 63,
+        city: "Palma de Mallorca"
+    },
+    {
+        name: "Marc Marquez",
+        number: 93,
+        team: "Repsol Honda Team",
+        nation: 'esp',
+        height: 168,
+        weight: 59,
+        city: "Cervera"
+    },
+    {
+        name: "Dani Pedrosa",
+        number: 26,
+        team: "Repsol Honda Team",
+        nation: 'esp',
+        height: 160,
+        weight: 51,
+        city: "Sabadell"
     }
+]
+
+var mainController = function ($scope) {
+    $scope.riders = riders;
 }
