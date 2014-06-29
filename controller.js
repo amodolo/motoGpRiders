234900883cb3a27c2e9724b516e0974ddab81e12
alessandro <alessandro.modolo.1988@gmail.com>
2014-06-29 14:27:09 +0200

    rispetto alo step 5, reperimento dei dati sui piloti tramite richiesta http

--- controller.js
+++ controller.js
@@ -1,43 +1,15 @@
-var riders = [
-    {
-        name: "Valentino Rossi",
-        number: 46,
-        team: "Movistar Yamaha MotoGP",
-        nation: 'ita',
-        height: 182,
-        weight: 65,
-        city: "Urbino"
-    },
-    {
-        name: "Jorge Lorenzo",
-        number: 99,
-        team: "Movistar Yamaha MotoGP",
-        nation: 'esp',
-        height: 173,
-        weight: 63,
-        city: "Palma de Mallorca"
-    },
-    {
-        name: "Marc Marquez",
-        number: 93,
-        team: "Repsol Honda Team",
-        nation: 'esp',
-        height: 168,
-        weight: 59,
-        city: "Cervera"
-    },
-    {
-        name: "Dani Pedrosa",
-        number: 26,
-        team: "Repsol Honda Team",
-        nation: 'esp',
-        height: 160,
-        weight: 51,
-        city: "Sabadell"
-    }
-]
-
 var controllerManager = angular.module('controllers', []);
+var riders;
+
+module.run(function($http){
+    $http.get('server/api/riders.json').
+        success(function(data){
+            riders=data;
+        }).
+        error(function(data, status){
+            alert("Si è verificato un errore nel caricamento della lista dei piloti.");
+        });
+});
 
 controllerManager.controller('ridersListController', function($scope){
     $scope.riders = riders;
